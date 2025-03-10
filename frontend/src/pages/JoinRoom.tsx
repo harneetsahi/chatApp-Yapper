131563f53e44aaa48923036199e543e2ea4e8c77
Harneet <harneetsahi8@gmail.com>
2025-03-09 23:40:19 -0500

    join room initial structure

--- frontend/src/pages/JoinRoom.tsx
+++ frontend/src/pages/JoinRoom.tsx
@@ -1,7 +1,29 @@
 function JoinRoom() {
   return (
     <>
-      <div>Join room</div>
+      <div>
+        <h1>Chat Room</h1>
+
+        <section>
+          <form action="">
+            <input
+              type="text"
+              name="roomId"
+              id="roomId"
+              placeholder="Room ID"
+            />
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="username"
+            />
+            <button>Join Room</button>
+          </form>
+          <p>OR</p>
+          <button>Create Room</button>
+        </section>
+      </div>
     </>
   );
 }
