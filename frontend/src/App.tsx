ef8e0e04a0a5c3d6618dc5a1cdf0e92dab36aede
Harneet <harneetsahi8@gmail.com>
2025-03-10 20:58:07 -0500

    css for chat room

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import JoinRoom from "./pages/JoinRoom";
+
 function App() {
   return (
     <>
-      <div> Home</div>
+      <div className="bg-stone-900 text-amber-50 h-screen w-screen">
+        <JoinRoom />
+      </div>
     </>
   );
 }
--- frontend/src/pages/JoinRoom.tsx
+++ frontend/src/pages/JoinRoom.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function JoinRoom() {
   const [roomCode, setRoomCode] = useState("");
+  const [status, setStatus] = useState("connecting...");
 
   function generateRoomCode() {
     const options = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
@@ -15,25 +16,31 @@ function JoinRoom() {
 
   return (
     <>
-      <div>
-        <h1>Chat Room</h1>
+      <div className="flex flex-col items-center h-full pt-30">
+        <h1 className="text-2xl">Chat Room</h1>
 
-        <section>
-          <p>Status: </p>
-          <form action="">
+        <section className="mt-10">
+          <p>
+            Status: <span className="text-green-500">{status}</span>{" "}
+          </p>
+          <form action="" className="mt-5 flex flex-col">
             <input
+              className="border-1 border-gray-700 px-4 py-2 rounded-lg w-100"
               type="text"
               name="roomId"
               id="roomId"
               placeholder="Room ID"
             />
             <input
+              className="border-1 border-gray-700 px-4 py-2 rounded-lg w-100 mt-5"
               type="text"
               name="username"
               id="username"
-              placeholder="username"
+              placeholder="Username"
             />
-            <button>Join Room</button>
+            <button className="border-1 border-gray-700 px-4 py-2 rounded-lg w-100 mt-5 bg-gray-200 text-gray-800 cursor-pointer">
+              Join Room
+            </button>
           </form>
           <p>OR</p>
           <button
