0010cadf2e143377a760d29cd0264236c6cd7552
Harneet <harneetsahi8@gmail.com>
2025-03-10 21:38:46 -0500

    added hover effect on input and button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -7,7 +7,7 @@ function Button({ text, onClick }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="border-1 border-gray-700 px-4 py-2 rounded-lg w-100  bg-gray-100 text-gray-800 cursor-pointer"
+      className="border-1 border-gray-700 px-4 py-2 rounded-lg w-100 bg-gray-100 text-gray-800 cursor-pointer hover:scale-x-103 transition-all"
     >
       {text}
     </button>
--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -8,7 +8,7 @@ interface InputProps {
 function Input({ type, name, id, placeholder }: InputProps) {
   return (
     <input
-      className="border-1 border-gray-700 px-4 py-2 rounded-lg w-100"
+      className="border-1 border-gray-700 px-4 py-2 rounded-lg w-100 hover:scale-x-103 transition-all"
       type={type}
       name={name}
       id={id}
