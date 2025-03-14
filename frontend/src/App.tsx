7252a148beb0edea3c8ef292da2bf9dd569a4ec0
Harneet <harneetsahi8@gmail.com>
2025-03-13 22:32:02 -0500

    adjusted route navigations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,48 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Home from "./pages/Home";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
+import Loader from "./components/Loader";
 
 function App() {
-  const { authUser, checkAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
+  if (isCheckingAuth && !authUser)
+    return (
+      <>
+        <div className="flex h-screen justify-center items-center ">
+          <Loader />
+        </div>
+      </>
+    );
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/signin" element={<Signin />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route
+            path="/"
+            element={authUser ? <Home /> : <Navigate to="/signin" />}
+          ></Route>
+          <Route
+            path="/signup"
+            element={!authUser ? <Signup /> : <Navigate to="/dashboard" />}
+          ></Route>
+          <Route
+            path="/signin"
+            element={!authUser ? <Signin /> : <Navigate to="/dashboard" />}
+          ></Route>
+          <Route
+            path="/dashboard"
+            element={authUser ? <Dashboard /> : <Navigate to="/signin" />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </>
