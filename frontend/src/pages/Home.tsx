46b9b4d7a98c67c5093b6105dc859688ce3e2603
Harneet <harneetsahi8@gmail.com>
2025-04-16 00:29:30 -0500

    Add: home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,18 @@
 function Home() {
   return (
     <>
-      <main className="flex">
-        <div>
-          <h1></h1>
+      <main className="flex items-center justify-center p-10 border-0">
+        <div className="m-20 border-0">
+          <p className="font-bold text-7xl/20">A place for</p>
+          <p className="font-bold text-7xl/20">the ones who</p>
+          <p className="font-bold text-8xl/20  text-indigo-500 ">
+            {" "}
+            love to talk
+          </p>
         </div>
-        <div></div>
+        <div className="border-0 w-90 ">
+          <p className=""> Yapper Illustration</p>
+        </div>
       </main>
     </>
   );
