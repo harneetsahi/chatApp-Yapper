444a2a496ca89802832b17898bde1429d6e9fafa
Harneet <harneetsahi8@gmail.com>
2025-04-22 18:02:44 -0500

    Add: improved cors handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
 import cors from "cors";
 
-import { app, server, io } from "./socket";
+import { app, server } from "./socket";
 import authRouter from "./routes/auth.route";
 import messageRouter from "./routes/message.route";
 import { connectDB } from "./db/db";
 
-dotenv.config();
-
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+app.use(express.json({ limit: "2mb" }));
+app.use(
+  express.urlencoded({ limit: "2mb", extended: true, parameterLimit: 2000 })
+);
+
 app.use(cookieParser());
 app.use(
   cors({
     origin:
-      `${process.env.NODE_ENV}` === "production"
+      process.env.NODE_ENV === "production"
         ? false
         : ["http://localhost:5173", "http://127.0.0.1:5173"],
-    methods: "GET,POST,PUT,DELETE",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   })
 );
 
