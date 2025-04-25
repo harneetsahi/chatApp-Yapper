9275cc9333bfa230f6756830dea462476bda6000
Harneet <harneetsahi8@gmail.com>
2025-04-25 16:07:13 -0500

    Add: word rotate motion

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,39 @@
+import { WordRotate } from "@/components/magicui/word-rotate";
+import { Link } from "react-router-dom";
+
+const textStyles = `font-medium text-3xl sm:text-4xl lg:text-5xl xl:text-6xl text-center`;
+
 function Home() {
   return (
     <>
-      <main className="flex items-center justify-center p-10 border-0">
-        <div className="m-20 border-0">
-          <p className="font-bold text-7xl/20">A place for</p>
-          <p className="font-bold text-7xl/20">the ones who</p>
-          <p className="font-bold text-8xl/20 text-indigo-500 ">
-            {" "}
-            love to talk
-          </p>
-        </div>
-        <div className="border-0 w-90 ">
-          <p className=""> Yapper Illustration</p>
+      <main>
+        <div className="max-w-[calc(1350px)] pt-50 h-[calc(100vh-200px)] ">
+          <div className=" transition-all flex flex-col gap-1 items-center justify-center ">
+            <p className={`${textStyles}`}>A place for</p>
+            <p className={`${textStyles}`}>people who</p>
+
+            <p
+              className={`text-center text-indigo-500 font-medium text-4xl sm:text-5xl lg:text-6xl xl:text-7xl mb-6 md:mb-8 `}
+            >
+              {" "}
+              <span>
+                <WordRotate
+                  words={["love to talk", "want to share", "stay in touch"]}
+                  duration={1800}
+                />
+              </span>
+            </p>
+
+            {/* {!authUser && ( */}
+            <Link
+              to="/signup"
+              className="  bg-indigo-500 text-white  py-1.5 md:px-5 px-4 rounded-3xl hover:scale-103 transition-all "
+              title="Sign up"
+            >
+              Get started
+            </Link>
+            {/* )} */}
+          </div>
         </div>
       </main>
     </>
