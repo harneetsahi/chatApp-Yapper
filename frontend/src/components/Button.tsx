a4c212554abde920b4495a9254e5f6b26a42622b
Harneet <harneetsahi8@gmail.com>
2025-04-25 16:03:28 -0500

    Add: name attr for accessibility

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -5,25 +5,27 @@ interface ButtonProps {
   text: string | React.JSX.Element;
   type?: "submit" | "button";
   disabled?: boolean;
+  name?: string;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const variantStyles = {
   primary:
-    "w-80 hover:scale-x-103 transition-all rounded-md px-4 py-2 bg-indigo-500 text-white  ",
+    "md:w-80 w-70 hover:scale-x-103 transition-all rounded-md px-4 py-2 bg-indigo-500 text-white ",
   chat: "rounded-md px-4 py-2",
   sendMessage: "bg-transparent border-none ",
 };
 
 const defaultStyles = "bg-blue-100 cursor-pointer";
 
-function Button({ variant, text, type, onClick, disabled }: ButtonProps) {
+function Button({ variant, text, type, onClick, disabled, name }: ButtonProps) {
   return (
     <button
       type={type}
       className={`${variantStyles[variant]} ${defaultStyles}`}
       onClick={onClick}
       disabled={disabled}
+      name={name}
     >
       {text}
     </button>
