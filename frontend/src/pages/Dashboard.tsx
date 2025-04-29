899fdf4fd2cf172a32fb8528738ffbbfb96a32fb
Harneet <harneetsahi8@gmail.com>
2025-04-29 16:53:49 -0500

    Add: default profile picture if user has not uploaded avatar

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,10 +2,11 @@ import Sidebar from "../components/Sidebar";
 import ChatBox from "../components/ChatBox";
 import NoChatBox from "../components/NoChatBox";
 import { useChatStore } from "../store/useChatStore";
-import { useAuthStore, User } from "../store/useAuthStore";
+import { useAuthStore } from "../store/useAuthStore";
 import { useEffect, useRef, useState } from "react";
 import Chevron from "../icons/Chevron";
 import { useOutsideClickSidebar } from "../hooks/useOutsideClick";
+import defaultProfile from "../public/profile.png";
 
 function Dashboard() {
   const { selectedUser } = useChatStore();
@@ -40,29 +41,30 @@ function Dashboard() {
 
   return (
     <>
-      <div className="h-[calc(100vh-82px)] max-w-[calc(1350px)] border-t-1 dark:border-zinc-800 border-indigo-100 flex m-auto relative">
+      <div className="h-[calc(100vh-72px)] max-w-[calc(1250px)] border-t-1 dark:border-zinc-800 border-indigo-100 flex m-auto relative">
         {/* sidebar */}
         <div ref={sidebarRef}>
           <button
             onClick={handleSidebar}
             title="Toggle Sidebar"
-            className={`flex justify-center items-center absolute z-30 top-3.5 left-2 w-8 h-8 bg-indigo-100 dark:bg-zinc-800 rounded-lg cursor-pointer `}
+            className={`flex justify-center items-center absolute z-30 top-3.5 left-2 w-8 h-8 cursor-pointer hover:text-indigo-500 `}
           >
             <Chevron
               className={`${
-                showSidebar && "rotate-180 transition-all duration-200"
+                showSidebar &&
+                "rotate-180 transition-all duration-200 -translate-x-0.5"
               }`}
             />
           </button>
           {showSidebar && (
             <Sidebar
-              className={`sm:relative absolute top-0 left-0 w-62 h-full z-20`}
+              className={`sm:relative absolute top-0 left-0 min-w-64 h-full z-20`}
             />
           )}
         </div>
 
         {/* chat area */}
-        <div className="flex-1">
+        <div className="flex-1 ">
           {!selectedUser ? (
             <NoChatBox />
           ) : (
@@ -71,13 +73,14 @@ function Dashboard() {
         </div>
 
         {/* profile section */}
-        <div className="w-62 hidden md:flex flex-col items-center gap-7 pt-20 ">
-          <p className="lg:text-2xl text-xl">Welcome, {authUser?.firstName}!</p>
+
+        <div className="min-w-50 lg:w-64 hidden lg:flex flex-col items-center gap-7 pt-20 border-l-1 dark:border-zinc-800 border-indigo-50 shadow-xs ">
+          <p className="text-lg">Welcome, {authUser?.firstName}!</p>
           <div className="relative">
             <img
-              src={authUser?.avatar}
+              src={authUser?.avatar || defaultProfile}
               alt=""
-              className="w-20 h-20 rounded-full bg-gray-300"
+              className="w-25 h-25 rounded-full shadow-sm"
             />
           </div>
         </div>
