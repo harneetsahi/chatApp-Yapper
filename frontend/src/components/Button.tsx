a2790247691f6683b824a2e04e0eb2d4165ccb5f
Harneet <harneetsahi8@gmail.com>
2025-04-29 16:51:51 -0500

    Add: global Button style

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 interface ButtonProps {
-  variant: "primary" | "secondary" | "sendMessage";
+  variant: "primary" | "secondary" | "sendMessage" | "delete";
   text: string | React.JSX.Element;
   type?: "submit" | "button";
   disabled?: boolean;
@@ -14,8 +14,9 @@ const variantStyles = {
     "md:w-80 w-70 hover:scale-x-103 transition-all rounded-md px-4 py-2 bg-indigo-500 text-white ",
   secondary:
     "md:w-80 w-70 rounded-sm py-1.5 md:px-6 px-4 text-center rounded-3xl bg-indigo-500 text-white hover:scale-103 transition-all",
-
   sendMessage: "bg-transparent border-none ",
+  delete:
+    "text-red-400 text-sm border-1 border-red-400 px-2 py-1 rounded-lg  hover:text-red-500 hover:border-red-500 transition-all",
 };
 
 const defaultStyles = " cursor-pointer";
