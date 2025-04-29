cb982a47a3112fa7de02ee275d20e7f6feedd841
Harneet <harneetsahi8@gmail.com>
2025-04-29 16:52:21 -0500

    Add: Update avatar option

--- frontend/src/components/SettingsDropdown.tsx
+++ frontend/src/components/SettingsDropdown.tsx
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom";
+
 function SettingsDropdown() {
   return (
     <>
-      <div className="rounded-xl w-45 dark:bg-zinc-800 bg-indigo-50 p-4 absolute right-0 top-10 z-10 shadow-xl border-0 border-indigo-100 dark:border-zinc-700/80">
-        <ul className="text-sm flex flex-col gap-2 text-left ">
-          <li className="dark:hover:bg-white/20 hover:bg-indigo-100/80 rounded-lg py-1 px-2">
-            Change Password
-          </li>
+      <div className="rounded-lg rounded-tr-none w-45 dark:bg-zinc-900 bg-white p-4 absolute right-4 top-10 z-10 shadow-sm ">
+        <ul className="text-sm flex flex-col gap-2 text-left">
+          <Link to="/updatePassword">
+            <li className="px-2 py-1  hover:scale-x-103 hover:text-indigo-500  transition-all">
+              Change Password
+            </li>
+          </Link>
+          <Link to="/updateProfile">
+            <li className="px-2 py-1 hover:scale-x-103 hover:text-indigo-500 transition-all ">
+              Update Avatar
+            </li>
+          </Link>
         </ul>
       </div>
     </>
