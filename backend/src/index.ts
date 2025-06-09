bd80f40663022ea0b8398932369c030ea4403c87
Harneet <harneetsahi8@gmail.com>
2025-06-09 17:57:19 -0500

    update: static file serving

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,13 +4,19 @@ dotenv.config();
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-import { app, server } from "./socket";
-import authRouter from "./routes/auth.route";
-import messageRouter from "./routes/message.route";
-import { connectDB } from "./db/db";
+import { app, server } from "./socket.js";
+import authRouter from "./routes/auth.route.js";
+import messageRouter from "./routes/message.route.js";
+import { connectDB } from "./db/db.js";
 
 const PORT = process.env.PORT || 3001;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.json({ limit: "2mb" }));
 app.use(
@@ -34,6 +40,14 @@ app.use(
 
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
